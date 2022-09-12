49fa310da0f0e11b7345429feec2dcc96aa90c2b
suleimanabubakar <suleiman.u.abubakar@gmail.com>
2022-09-12 15:58:06 +0300

    organisation administratives

--- organisations/admin.py
+++ organisations/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["name",]
+@admin.register(Organisation)
+class Admin(admin.ModelAdmin):
+    list_display = ["name","address","created_on","created_by","type"]
+
+@admin.register(OrganisationOfficial)
+class OrganisationOfficialAdmin(admin.ModelAdmin):
+    list_display = ["user","organisation","assigned_on"]
--- trees/admin.py
+++ trees/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import *
 
 # Register your models here.
+
+
+@admin.register(Tree)
+class TreeAdmin(admin.ModelAdmin):
+    list_display = ["created_on","planter","location","height","image"]
